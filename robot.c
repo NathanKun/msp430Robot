40b2ea66dfe49ca3a851e45e6b949f8b985cb060
JunyangHE <junyang.he@groupe-esigelec.org>
2017-01-05 15:21:42 +0100

    Project started. Created robot.h robot.c. Added some basic functions

---
+++ robot.c
@@ -0,0 +1,126 @@
+/*
+ * robot.c
+ *
+ *  Created on: 5 janv. 2017
+ *      Author: j.he.12
+ */
+#include "robot.h"
+
+void initRobotPort1() {
+	// TODO wait for design
+}
+
+void initRobotPort2() {
+	P2DIR = 0x36;
+	// TODO P26, P27 unknown
+}
+
+void initMotor() {
+	// for sure
+	TA0CTL = 0;
+	TA0CCR0 = 0;
+	TA0R = 0;
+	TA0CTL &= ~TAIFG; // reset flag
+	TA0CTL = UP_MODE | TASSEL_2 | ID_0; // use SMCLK
+	TA0CCTL1 |= OUTMOD_7;
+	TA0CCR0 = 1000;
+	TA0CCR1 = 0; // speed = 0
+
+	TA1CTL = 0;
+	TA1CCR0 = 0;
+	TA1R = 0;
+	TA1CTL &= ~TAIFG;
+	TA1CTL = UP_MODE | TASSEL_2 | ID_0; // use SMCLK
+	TA1CCTL1 |= OUTMOD_7;
+	TA1CCR0 = 1000;
+	TA1CCR1 = 0;
+
+	selectPin(MOTORAPWMPIN, 1);
+	selectPin(MOTORBPWMPIN, 1);
+}
+
+void initRobot() {
+	initPorts();
+	initRobotPort2();
+	initRobotPort2();
+	initMotor();
+}
+
+void setMortorSpeed(motor motor, int speed) {
+	if (motor == motorA) {
+		TA0CCR1 = 10 * speed;
+	} else if (motor == motorB) {
+		TA1CCR1 = 10 * speed;
+	}
+}
+
+void setMortorDirection(motor motor, direction d) {
+	if (motor == motorA) {
+		if(d == FORWARD) {
+			digitalWrite(MOTORADIRPIN, HIGH);
+		} else if (d == BACK) {
+			digitalWrite(MOTORADIRPIN, LOW);
+		}
+	} else if (motor == motorB) {
+		if(d == FORWARD) {
+			digitalWrite(MOTORBDIRPIN, HIGH);
+		} else if (d == BACK) {
+			digitalWrite(MOTORBDIRPIN, LOW);
+		}
+	}
+}
+
+int readOpto(opto opto) {
+	if (opto == optoA)
+		return digitalRead(P20);
+	else if (opto == optoB)
+		return digitalRead(P23);
+	return 0;
+}
+
+void goForward(int speed) {
+	setMortorDirection(motorA, FORWARD);
+	setMortorDirection(motorB, FORWARD);
+	setMortorSpeed(motorA, speed);
+	setMortorSpeed(motorB, speed);
+}
+
+void goBack(int speed) {
+	setMortorDirection(motorA, BACK);
+	setMortorDirection(motorB, BACK);
+	setMortorSpeed(motorA, speed);
+	setMortorSpeed(motorB, speed);
+}
+
+void turnLeft(int speed) {
+	setMortorDirection(motorA, BACK);
+	setMortorDirection(motorB, FORWARD);
+	setMortorSpeed(motorA, speed);
+	setMortorSpeed(motorB, speed);
+}
+
+void turnRight(int speed) {
+	setMortorDirection(motorA, FORWARD);
+	setMortorDirection(motorB, BACK);
+	setMortorSpeed(motorA, speed);
+	setMortorSpeed(motorB, speed);
+}
+
+void goForwardTurn(int speedA, int speedB) {
+	setMortorDirection(motorA, FORWARD);
+	setMortorDirection(motorB, FORWARD);
+	setMortorSpeed(motorA, speedA);
+	setMortorSpeed(motorB, speedB);
+}
+
+void goBackTurn(int speedA, int speedB) {
+	setMortorDirection(motorA, BACK);
+	setMortorDirection(motorB, BACK);
+	setMortorSpeed(motorA, speedA);
+	setMortorSpeed(motorB, speedB);
+}
+
+void stopRobot() {
+	setMortorSpeed(motorA, 0);
+	setMortorSpeed(motorB, 0);
+}
---
+++ robot.h
@@ -0,0 +1,59 @@
+/*
+ * robot.h
+ *
+ *  Created on: 5 janv. 2017
+ *      Author: j.he.12
+ */
+
+/**
+ * P1.0-P1.7 Connectés au bornier vert (cf nomenclature sur CI) Entrées / Sorties
+ * P 2.0 Opto-coupleur roue A 									Entrée
+ * P 2.1 Sens pour le moteur A 									Sortie
+ * P 2.2 PWM pour le moteur A, 250 KHz maximum 					Sortie
+ * P 2.3 Opto-coupleur roue B 									Entrée
+ * P 2.4 PWM pour le moteur B, 250 KHz maximum 					Sortie
+ * P 2.5 Sens pour le moteur B 									Sortie
+ */
+
+#ifndef ROBOT_H_
+#define ROBOT_H_
+
+#include "simpleMSP.h"
+
+#define CPU_F ((double)1000000)
+#define delay_us(x) __delay_cycles((long)(CPU_F*(double)x/1000000.0))
+#define delay_ms(x) __delay_cycles((long)(CPU_F*(double)x/1000.0))
+
+#define MOTORAPWMPIN P21
+#define MOTORBPWMPIN P24
+#define MOTORADIRPIN P21
+#define MOTORBDIRPIN P24
+
+// direction enum
+typedef enum {
+FORWARD = HIGH, BACK = LOW
+} direction;
+
+// motor enum
+typedef enum {
+	motorA = 0, motorB = 1
+} motor;
+
+// opto-isoler enum
+typedef enum {
+	optoA = 0, optoB = 1
+} opto;
+
+void initRobot();
+void setMortorSpeed(motor motor, int speed);
+void setMortorDirection(motor motor, direction d);
+int readOpto(opto opto);
+
+
+
+
+
+
+
+
+#endif /* ROBOT_H_ */
